83c484ab7017047f0a1814704cd6ed714122fbea
Luca Fluri <luca.fluri@fhnw.ch>
2023-12-02 22:25:31 +0100

    add non linq version

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -3,6 +3,9 @@ using System.Text.RegularExpressions;
 
 namespace AdventOfCode;
 
+
+//SOLVED IN PYTHON
+
 public class Day01 : BaseDay
 {
     private readonly string _input;
@@ -36,20 +39,42 @@ public class Day01 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        var total = (from line in _inputLines 
-            select Regex.Replace(line, "one", "o1ne") into l 
-            select Regex.Replace(l, "two", "t2wo") into l 
-            select Regex.Replace(l, "three", "t3hree") into l 
-            select Regex.Replace(l, "four", "f4our") into l 
-            select Regex.Replace(l, "five", "f5ive") into l 
-            select Regex.Replace(l, "six", "s6ix") into l 
-            select Regex.Replace(l, "seven", "s7even") into l 
-            select Regex.Replace(l, "eight", "e8ight") into l 
-            select Regex.Replace(l, "nine", "n9ine") into l 
-            let rgx = new Regex(@"[^\d]") 
-            select rgx.Replace(l, "") into l 
-            select int.Parse(l[0] + l[l.Length - 1].ToString())).Sum();
+        var total = 0;
+        
+        foreach (var line in _inputLines)
+        {
+            string l = Regex.Replace(line, "one", "o1ne");
+            l = Regex.Replace(l, "two", "t2wo");
+            l = Regex.Replace(l, "three", "t3hree");
+            l = Regex.Replace(l, "four", "f4our");
+            l = Regex.Replace(l, "five", "f5ive");
+            l = Regex.Replace(l, "six", "s6ix");
+            l = Regex.Replace(l, "seven", "s7even");
+            l = Regex.Replace(l, "eight", "e8ight");
+            l = Regex.Replace(l, "nine", "n9ine");
+            
+            Regex rgx = new Regex(@"[^\d]");
+            l = rgx.Replace(l, "");
+        
+            total += int.Parse(l[0] + l[l.Length- 1].ToString());
+        }
+        
+        // LINQ version
+        // var total = (from line in _inputLines 
+        //     select Regex.Replace(line, "one", "o1ne") into l 
+        //     select Regex.Replace(l, "two", "t2wo") into l 
+        //     select Regex.Replace(l, "three", "t3hree") into l 
+        //     select Regex.Replace(l, "four", "f4our") into l 
+        //     select Regex.Replace(l, "five", "f5ive") into l 
+        //     select Regex.Replace(l, "six", "s6ix") into l 
+        //     select Regex.Replace(l, "seven", "s7even") into l 
+        //     select Regex.Replace(l, "eight", "e8ight") into l 
+        //     select Regex.Replace(l, "nine", "n9ine") into l 
+        //     let rgx = new Regex(@"[^\d]") 
+        //     select rgx.Replace(l, "") into l 
+        //     select int.Parse(l[0] + l[l.Length - 1].ToString())).Sum();
 
-        return new ValueTask<string>(total.ToString()); //54518
+
+        return new ValueTask<string>(total.ToString());
     }
 }
