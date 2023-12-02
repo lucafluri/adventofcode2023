925c628f5e3da19226392a48fb2e2b2af98d2c6c
Luca Fluri <luca.fluri@fhnw.ch>
2023-12-02 14:20:25 +0100

    solved day 02

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,34 @@
-﻿namespace AdventOfCode;
+﻿using System.Diagnostics;
+
+namespace AdventOfCode;
+
+
+//SOLVED IN PYTHON
 
 public class Day01 : BaseDay
 {
     private readonly string _input;
+    private readonly string[] _inputLines;
 
     public Day01()
     {
         _input = File.ReadAllText(InputFilePath);
+        _inputLines = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+
+    public override ValueTask<string> Solve_1()
+    {
+        Console.WriteLine("");
+        Console.WriteLine(_inputLines.Length);
+
+        foreach (var line in _inputLines)
+        {
+        }
+        
+        return new ValueTask<string>("RESULT");
+    }
+    // public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
 }
---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,141 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public class Day02 : BaseDay
+{
+    private readonly string _input;
+    private readonly string[] _inputLines;
+    private List<Game> games = new List<Game>();
+
+    private class Game
+    {
+        public int id { get; set; }
+        
+       
+        public int minRed { get; set; }
+        public int minGreen { get; set; }
+        public int minBlue { get; set; }
+        public int setCount { get; set; }
+        
+        public string[] sets { get; set; }
+        
+        public bool isValid = true;
+        
+    }
+
+    public Day02()
+    {
+        _input = File.ReadAllText(InputFilePath);
+        _inputLines = File.ReadAllLines(InputFilePath);
+    }
+
+    private void ParseGames(string[] lines)
+    {
+        int id = 0;
+        foreach (var line in lines)
+        {
+            id++;
+            string l = line;
+            Game g = new Game();
+            g.minRed = 0;
+            g.minGreen = 0;
+            g.minBlue = 0;
+            
+            g.id = id;
+             
+            // Parse Line into sets
+            //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+
+            // Remove Game 1: with regex
+            Regex rgx = new Regex(@"Game \d+:");
+            l = rgx.Replace(l, "");
+            
+            // Remove all spaces
+            l = l.Replace(" ", "");
+            
+            // Split into sets by ;
+            string[] sets = l.Split(";");
+            
+            g.setCount = sets.Length;
+            g.sets = sets;
+
+            foreach (var set in sets)
+            {
+                var cubes = set.Split(",");
+                foreach (var c in cubes)
+                {
+                    if (c.Contains("red"))
+                    {
+                        int redCount = int.Parse(c.Replace("red", ""));
+                        if (redCount > 12)
+                        {
+                            g.isValid = false;
+                        }
+                        
+                        if(redCount > g.minRed)
+                            g.minRed = redCount;
+                    }
+                    if (c.Contains("green"))
+                    {
+                        int greenCount = int.Parse(c.Replace("green", ""));
+                        if (greenCount > 13)
+                        {
+                            g.isValid = false;
+                        }
+                        if(greenCount > g.minGreen)
+                            g.minGreen = greenCount;
+                    }
+                    if (c.Contains("blue"))
+                    {
+                        int blueCount = int.Parse(c.Replace("blue", ""));
+                        if (blueCount > 14)
+                        {
+                            g.isValid = false;
+                        }
+                        if(blueCount > g.minBlue)
+                            g.minBlue = blueCount;
+                    }
+                }
+            }
+            games.Add(g);
+        }
+    }
+
+
+    public override ValueTask<string> Solve_1()
+    {
+        Console.WriteLine("");
+        
+        ParseGames(_inputLines);
+        
+        int total = 0;
+
+        foreach (var game in games)
+        {
+            if (game.isValid)
+            {
+                total += game.id;
+            }
+        }
+        
+        return new ValueTask<string>(total.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int total = 0;
+
+        foreach (var game in games) 
+        {
+            int power = game.minRed * game.minGreen * game.minBlue;
+            total += power;
+            
+            // Console.WriteLine($"{game.id} - {game.minRed} - {game.minGreen} - {game.minBlue} - {power}");
+        }
+        
+        return new ValueTask<string>(total.ToString());
+        
+    }
+}
