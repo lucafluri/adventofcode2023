92fdf70db8e9eb76a0fea18e5489768f6f224608
Luca Fluri <luca.fluri@fhnw.ch>
2023-12-04 08:16:58 +0100

    small cleanup 04

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -13,6 +13,19 @@ public class Day04 : BaseDay
     {
         _input = File.ReadAllLines(InputFilePath);
     }
+
+    private int getWonCount(string line)
+    {
+        string inp = line;
+        inp = Regex.Replace(inp, @"\s+", " ");
+        Regex rgx = new Regex(".*:(.*)\\|(.*)");
+        var match = rgx.Match(inp);
+        List<int> nums = new List<int>();
+        var l1 = match.Groups[1].ToString().Trim().Split(" ").ToList();
+        var l2 = match.Groups[2].ToString().Trim().Split(" ").ToList();
+            
+        return l1.Intersect(l2).Count();
+    }
     
     
 
@@ -20,19 +33,9 @@ public class Day04 : BaseDay
     {
         foreach (var line in _input)
         {
-            string inp = line;
-            inp = Regex.Replace(inp, @"\s+", " ");
-            Regex rgx = new Regex(".*:(.*)\\|(.*)");
-            var match = rgx.Match(inp);
-            List<int> nums = new List<int>();
-            var l1 = match.Groups[1].ToString().Trim().Split(" ").ToList();
-            var l2 = match.Groups[2].ToString().Trim().Split(" ").ToList();
-            
-            
-            var l3 = l1.Intersect(l2);
-            if(l3.Any())
-                total += (int) Math.Floor(Math.Pow(2, l3.Count()-1));    
-            // nums = match.Groups[1].ToString().Trim().Split(" ").ToList();
+            var c = getWonCount(line); 
+            if(c != 0)
+                total += (int) Math.Floor(Math.Pow(2, c-1));    
         }
        
         return new ValueTask<string>(total.ToString());
@@ -50,18 +53,8 @@ public class Day04 : BaseDay
         foreach (var line in _input)
         {
             idx++;
-            
-            string inp = line;
-            inp = Regex.Replace(inp, @"\s+", " ");
-            Regex rgx = new Regex(".*:(.*)\\|(.*)");
-            var match = rgx.Match(inp);
-            var l1 = match.Groups[1].ToString().Trim().Split(" ").ToList();
-            var l2 = match.Groups[2].ToString().Trim().Split(" ").ToList();
-            
-            var l3 = l1.Intersect(l2);
-            var l3Ints = l3.Select(int.Parse).ToList();
-            //each num from idx to idx + l3Ints.Count() as list
-            var count = l3.Count();
+
+            var count = getWonCount(line);
             var wins = new List<int>();
             for(var i = idx+1; i <= idx + count; i++)
             {
@@ -86,7 +79,6 @@ public class Day04 : BaseDay
             }
         }
         
-
         total = cards.Sum();
         
         return new ValueTask<string>(total.ToString());
