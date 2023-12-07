022807d40fa00d8d7f4bd25e5e203767beb2bae1
Luca Fluri <luca.fluri@fhnw.ch>
2023-12-07 20:03:54 +0100

    optimize 02

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -12,10 +12,11 @@ public class Day02 : BaseDay
     private readonly string _input;
     private readonly string[] _inputLines;
     private List<Game> games = new List<Game>();
+    private Regex rgx = new(@"Game \d+:");
 
     private class Game
     {
-        public int id { get; set; }
+        public int id { get; init; }
         
        
         public int minRed { get; set; }
@@ -36,32 +37,30 @@ public class Day02 : BaseDay
         ParseGames(_inputLines);
     }
 
-    private void ParseGames(string[] lines)
+    private void ParseGames(IEnumerable<string> lines)
     {
-        int id = 0;
+        var id = 0;
         foreach (var line in lines)
         {
             id++;
-            string l = line;
-            Game g = new Game();
-            g.minRed = 0;
-            g.minGreen = 0;
-            g.minBlue = 0;
-            
-            g.id = id;
-             
+            var l = line;
+            var g = new Game
+            {
+                minRed = 0,
+                minGreen = 0,
+                minBlue = 0,
+                id = id
+            };
+
             // Parse Line into sets
             //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 
             // Remove Game 1: with regex
-            Regex rgx = new Regex(@"Game \d+:");
             l = rgx.Replace(l, "");
-            
-            // Remove all spaces
             l = l.Replace(" ", "");
             
             // Split into sets by ;
-            string[] sets = l.Split(";");
+            var sets = l.Split(";");
             
             g.setCount = sets.Length;
             g.sets = sets;
@@ -73,7 +72,7 @@ public class Day02 : BaseDay
                 {
                     if (c.Contains("red"))
                     {
-                        int redCount = int.Parse(c.Replace("red", ""));
+                        var redCount = int.Parse(c.Replace("red", ""));
                         if (redCount > 12)
                         {
                             g.isValid = false;
@@ -84,7 +83,7 @@ public class Day02 : BaseDay
                     }
                     if (c.Contains("green"))
                     {
-                        int greenCount = int.Parse(c.Replace("green", ""));
+                        var greenCount = int.Parse(c.Replace("green", ""));
                         if (greenCount > 13)
                         {
                             g.isValid = false;
@@ -92,16 +91,15 @@ public class Day02 : BaseDay
                         if(greenCount > g.minGreen)
                             g.minGreen = greenCount;
                     }
-                    if (c.Contains("blue"))
+
+                    if (!c.Contains("blue")) continue;
+                    var blueCount = int.Parse(c.Replace("blue", ""));
+                    if (blueCount > 14)
                     {
-                        int blueCount = int.Parse(c.Replace("blue", ""));
-                        if (blueCount > 14)
-                        {
-                            g.isValid = false;
-                        }
-                        if(blueCount > g.minBlue)
-                            g.minBlue = blueCount;
+                        g.isValid = false;
                     }
+                    if(blueCount > g.minBlue)
+                        g.minBlue = blueCount;
                 }
             }
             games.Add(g);
@@ -111,34 +109,15 @@ public class Day02 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        Console.WriteLine("");
-        
-        
-        int total = 0;
+        var total = games.Where(game => game.isValid).Sum(game => game.id);
 
-        foreach (var game in games)
-        {
-            if (game.isValid)
-            {
-                total += game.id;
-            }
-        }
-        
         return new ValueTask<string>(total.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        int total = 0;
+        var total = games.Sum(game => game.minRed * game.minGreen * game.minBlue);
 
-        foreach (var game in games) 
-        {
-            int power = game.minRed * game.minGreen * game.minBlue;
-            total += power;
-            
-            // Console.WriteLine($"{game.id} - {game.minRed} - {game.minGreen} - {game.minBlue} - {power}");
-        }
-        
         return new ValueTask<string>(total.ToString());
         
     }
