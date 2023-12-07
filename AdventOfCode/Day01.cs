0c3c1b286ffe8963a9a590463a371ae925441359
lucafluri <me@lucafluri.ch>
2023-12-07 22:49:44 +0100

    less regexy version for 01

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -10,18 +10,20 @@ namespace AdventOfCode;
 public partial class Day01 : BaseDay
 {
     private List<string> _inputLines;
-    private readonly Regex _rgx = new(@"[^\d]"); // Matches everything except nums
-    private readonly Regex _rgx1 = new(@"one"); 
-    private readonly Regex _rgx2 = new(@"two"); 
-    private readonly Regex _rgx3 = new(@"three"); 
-    private readonly Regex _rgx4 = new(@"four"); 
-    private readonly Regex _rgx5 = new(@"five"); 
-    private readonly Regex _rgx6 = new(@"six"); 
-    private readonly Regex _rgx7 = new(@"seven"); 
-    private readonly Regex _rgx8 = new(@"eight"); 
-    private readonly Regex _rgx9 = new(@"nine"); 
-        
     private int _total = 0;
+    private readonly Regex _rgx = new(@"[^\d]"); // Matches everything except nums
+    private readonly Dictionary<string, string> nums = new Dictionary<string, string>
+    {
+        {"one", "on1e"},
+        {"two", "t2wo"},
+        {"three", "t3hree"},
+        {"four", "f4our"},
+        {"five", "f5ive"},
+        {"six", "s6ix"},
+        {"seven", "s7even"},
+        {"eight", "e8ight"},
+        {"nine", "n9ine"}
+    };
 
     public Day01()
     {
@@ -42,19 +44,13 @@ public partial class Day01 : BaseDay
         
         foreach (var line in _inputLines)
         {
-            var l = _rgx1.Replace(line, "on1e");
-            l = _rgx2.Replace(l, "t2wo");
-            l = _rgx3.Replace(l,  "t3hree");
-            l = _rgx4.Replace(l,  "f4our");
-            l = _rgx5.Replace(l,  "f5ive");
-            l = _rgx6.Replace(l,  "s6ix");
-            l = _rgx7.Replace(l,  "s7even");
-            l = _rgx8.Replace(l,  "e8ight");
-            l = _rgx9.Replace(l,  "n9ine");
+            var l = line;
+            foreach(var entry in nums)
+                l = l.Replace(entry.Key, entry.Value);
             
             l = _rgx.Replace(l, "");
             _total += int.Parse(l[0] + l[^1].ToString());
         }
-        return new(_total.ToString());
+        return new(_total.ToString()); // 54518
     }
 }
