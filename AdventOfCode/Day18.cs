37d9b4e051eeeacb384ad14349404593ef4017e8
Luca Fluri <luca.fluri@fhnw.ch>
2023-12-18 16:00:25 +0100

    solve 18

--- AdventOfCode/Day18.cs
+++ AdventOfCode/Day18.cs
@@ -3,171 +3,79 @@
 namespace AdventOfCode;
 
 // Runtime Total: ~Xms
-// Setup: ~Xms
+// Setup: ~Xms 
 // Part 1: ~Xms
 // Part 2: ~Xms
 public class Day18 : BaseDay
 {
     private List<string> _input;
     private static readonly Regex _rgxline = new(@"([UDLR])\s(\d+)\s\(#(.{6})\)");
-    private Dictionary<(int x, int y), string> _loop = new();
-    private List<(int, int)> _sorted = new();
-    int minX, minY, maxX, maxY;
+    private readonly List<(int x, int y)> _loop2 = [];
+    private readonly List<(int x, int y)> _loop = [];
     
     public Day18()
     {
         _input = File.ReadAllLines(InputFilePath).ToList();
-        
-        var x = 0;
-        var y = 0;
-        
-        (minX, minY, maxX, maxY) = (x, y, x, y);
-        foreach (var line in _input)
+
+        foreach (var match in _input.Select(line => _rgxline.Match(line)))
         {
-            var match = _rgxline.Match(line);
             var dir = match.Groups[1].Value;
             var steps = int.Parse(match.Groups[2].Value);
             var color = match.Groups[3].Value;
-            
-            for (var i = 0; i < steps; i++)
-            {
-                switch (dir)
-                {
-                    case "U":
-                        y--;
-                        break;
-                    case "D":
-                        y++;
-                        break;
-                    case "L":
-                        x--;
-                        break;
-                    case "R":
-                        x++;
-                        break;
-                }
-                
-                if (x < minX)
-                    minX = x;
-                if (x > maxX)
-                    maxX = x;
-                if (y < minY)
-                    minY = y;
-                if (y > maxY)
-                    maxY = y;
 
-                if (!_loop.ContainsKey((x, y)))
-                    _loop.Add((x, y), color);
-            }
+            var steps2 = Convert.ToInt64(color.Substring(0, 5), 16);
+            var dir2 = color.Substring(5, 1);
+
+            AddSteps(dir, steps, _loop);
+            AddSteps(dir2, steps2, _loop2);
         }
-         
-        // Console.WriteLine(_loop.Count);
-        // Console.WriteLine($"{string.Join(", ", _loop.Keys.Select(x => $"({x.x}, {x.y})"))}");
-        // Console.WriteLine($"x: {minX} - {maxX}, y: {minY} - {maxY}");
-        
-        Shift();
-        // Console.WriteLine($"{string.Join(", ", _loop.Keys.Select(x => $"({x.x}, {x.y})"))}");
-        // Console.WriteLine($"x: {minX} - {maxX}, y: {minY} - {maxY}"); 
-        
-        _sorted = _loop.Keys.ToList();
-        _sorted = _sorted.OrderBy(x => x.Item1).ThenBy(a => a.Item2).ToList();
-        // Console.WriteLine($"{string.Join(", ", _sorted.Select(x => $"({x.Item1}, {x.Item2})"))}");
-        
+    }
+
+    private static void AddSteps(string dir, long steps, List<(int, int)> loop)
+    {
+        var (x, y) = (0, 0);
+        for (var i = 0; i < steps; i++)
+        {
+            switch (dir)
+            {
+                case "U":
+                case "3": y--; break;
+                case "D":
+                case "1": y++; break;
+                case "L": 
+                case "2": x--; break;
+                case "R": 
+                case "0": x++; break;
+            }
+            loop.Add((x, y));
+        }
     }
      
-    private void Shift()
+    private static long CalculateArea(IReadOnlyList<(int x, int y)> vertices)
     {
-        var newLoop = new Dictionary<(int x, int y), string>();
-        foreach (var (key, value) in _loop)
+        double area = 0;
+        var n = vertices.Count;
+        for (var i = 0; i < n; i++)
         {
-            newLoop.Add((key.x - minX, key.y - minY), value);
+            var j = (i + 1) % n;
+            area += (vertices[i].x * vertices[j].y) - (vertices[j].x * vertices[i].y);
         }
-
-        _loop = newLoop;
-        (minX, minY, maxX, maxY) = (0, 0, maxX - minX, maxY - minY);
-    }
-    
-    // Calculate inner area of loop defined by 2d points in _sorted
-    int CalculateArea()
-    {
-        var points = _sorted.Select(x => (x.Item1, x.Item2)).ToList();
-        // Determine the dimensions of the grid
-
-        bool[,] grid = new bool[maxX + 1, maxY + 1];
-        foreach (var (x, y) in points)
-        {
-            grid[x, y] = true;
-        }
-
-        // Find a starting point for the flood fill
-        (int, int) start = FindStartPoint(grid, maxX, maxY);
-        if (start == (-1, -1))
-        {
-            return 0; // No starting point found, possibly an invalid loop
-        }
-
-        // Perform flood fill
-        return FloodFill(grid, start.Item1, start.Item2, maxX, maxY);
+        return (long)(Math.Abs(area) / 2.0);
     }
 
-    static (int, int) FindStartPoint(bool[,] grid, int maxX, int maxY)
+    private static long CalculateInteriorPoints(IReadOnlyList<(int x, int y)> vertices)
     {
-        for (int x = 1; x < maxX; x++)
-        {
-            for (int y = 1; y < maxY; y++)
-            {
-                if (!grid[x, y])
-                {
-                    return (x, y);
-                }
-            }
-        }
-        return (-1, -1);
+        var area = CalculateArea(vertices);
+        return (long)(area + 1 + vertices.Count / 2.0);
     }
-
-    static int FloodFill(bool[,] grid, int startX, int startY, int maxX, int maxY)
-    {
-        Queue<(int, int)> queue = new Queue<(int, int)>();
-        queue.Enqueue((startX, startY));
-
-        int area = 0;
-
-        while (queue.Count > 0)
-        {
-            (int x, int y) = queue.Dequeue();
-
-            if (x < 0 || x > maxX || y < 0 || y > maxY || grid[x, y])
-            {
-                continue;
-            }
-
-            grid[x, y] = true;
-            area++;
-
-            queue.Enqueue((x + 1, y));
-            queue.Enqueue((x - 1, y));
-            queue.Enqueue((x, y + 1));
-            queue.Enqueue((x, y - 1)); 
-        } 
-
-        return area;
-    }
-    
-    
-    
 
     public override ValueTask<string> Solve_1()
     {        
-        var innerArea = CalculateArea();
-        var area = _loop.Count + CalculateArea();
-        Console.WriteLine($"{_loop.Count} + {innerArea} = {area}");
-        return new (area.ToString());
-        // L-23583
-    }
+        return new (CalculateInteriorPoints(_loop).ToString()); // 46334
+    } 
 
     public override ValueTask<string> Solve_2()
     {
-        
-        return new (0.ToString());
+        return new (CalculateInteriorPoints(_loop2).ToString()); // 102000662718092
     } 
 } 
